b385965ddf6ae8112676b83fee7dff7bdffd413f
superfishal <97133716+superfishal@users.noreply.github.com>
2022-10-25 15:29:42 -0700

    checking elements sent thru headless browser

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -86,7 +86,15 @@ suite("Functional Tests with Zombie.js", function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
+      browser.fill("surname", "Colombo").then(() => {
+        browser.pressButton("submit", () => {
+          browser.assert.success();
+          browser.assert.text("span#name", "Cristoforo");
+          browser.assert.text("span#name", "Colombo");
+          browser.assert.elements("spann#dates", 1);
+          done();
+        });
+      });
 
       done();
     });
