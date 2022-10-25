ec2cc37992c3b322aaf5b69eaa3cc88f1033488d
superfishal <97133716+superfishal@users.noreply.github.com>
2022-10-25 13:02:50 -0700

    test asserts for json object

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -50,9 +50,20 @@ suite("Functional Tests", function () {
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
+      chai
+        .request(server)
+        .put("/travellers")
+        .send({
+          surname: "Colombo",
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, "application/json");
+          assert.equal(res.body.name, "Cristoforo");
+          assert.equal(res.body.surname, "Colombo");
 
-      done();
+          done();
+        });
     });
   });
 });
