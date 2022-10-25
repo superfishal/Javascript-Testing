a4ee98715eea9ccfa01ce8ae4602ef9f87203da6
superfishal <97133716+superfishal@users.noreply.github.com>
2022-10-25 11:50:26 -0700

    check status and response text equal

--- server.js
+++ server.js
@@ -1,113 +1,114 @@
-'use strict'
-const express = require('express');
+"use strict";
+const express = require("express");
 const app = express();
 
-const cors = require('cors');
-const runner = require('./test-runner');
+const cors = require("cors");
+const runner = require("./test-runner");
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-})
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/hello', function (req, res) {
-  const name = req.query.name || 'Guest';
-  res.type('txt').send('hello ' + name);
-})
+app.get("/hello", function (req, res) {
+  const name = req.query.name || "Guest";
+  res.type("txt").send("hello " + name);
+});
 
 const travellers = function (req, res) {
   let data = {};
   if (req.body && req.body.surname) {
     switch (req.body.surname.toLowerCase()) {
-      case 'polo':
+      case "polo":
         data = {
-          name: 'Marco',
-          surname: 'Polo',
-          dates: '1254 - 1324'
+          name: "Marco",
+          surname: "Polo",
+          dates: "1254 - 1324",
         };
         break;
-      case 'colombo':
+      case "colombo":
         data = {
-          name: 'Cristoforo',
-          surname: 'Colombo',
-          dates: '1451 - 1506'
+          name: "Cristoforo",
+          surname: "Colombo",
+          dates: "1451 - 1506",
         };
         break;
-      case 'vespucci':
+      case "vespucci":
         data = {
-          name: 'Amerigo',
-          surname: 'Vespucci',
-          dates: '1454 - 1512'
+          name: "Amerigo",
+          surname: "Vespucci",
+          dates: "1454 - 1512",
         };
         break;
-      case 'da verrazzano':
-      case 'verrazzano':
+      case "da verrazzano":
+      case "verrazzano":
         data = {
-          name: 'Giovanni',
-          surname: 'da Verrazzano',
-          dates: '1485 - 1528'
+          name: "Giovanni",
+          surname: "da Verrazzano",
+          dates: "1485 - 1528",
         };
         break;
       default:
         data = {
-          name: 'unknown'
-        }
+          name: "unknown",
+        };
     }
   }
   res.json(data);
 };
 
-
-app.route('/travellers')
-  .put(travellers);
+app.route("/travellers").put(travellers);
 
 let error;
-app.get('/_api/get-tests', cors(), function (req, res, next) {
-  if (error)
-    return res.json({ status: 'unavailable' });
-  next();
-},
+app.get(
+  "/_api/get-tests",
+  cors(),
+  function (req, res, next) {
+    if (error) return res.json({ status: "unavailable" });
+    next();
+  },
   function (req, res, next) {
     if (!runner.report) return next();
     res.json(testFilter(runner.report, req.query.type, req.query.n));
   },
   function (req, res) {
-    runner.on('done', function (report) {
-      process.nextTick(() => res.json(testFilter(runner.report, req.query.type, req.query.n)));
+    runner.on("done", function (report) {
+      process.nextTick(() =>
+        res.json(testFilter(runner.report, req.query.type, req.query.n))
+      );
     });
-  });
-
+  }
+);
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("Listening on port " + port);
-  console.log('Running Tests...');
+  console.log("Running Tests...");
   setTimeout(function () {
     try {
       runner.run();
     } catch (e) {
       error = e;
-      console.log('Tests are not valid:');
+      console.log("Tests are not valid:");
       console.log(error);
     }
   }, 1500);
 });
-
 
 module.exports = app; // for testing
 
 function testFilter(tests, type, n) {
   let out;
   switch (type) {
-    case 'unit':
-      out = tests.filter(t => t.context.match('Unit Tests'));
-      break;
-    case 'functional':
-      out = tests.filter(t => t.context.match('Functional Tests'));
+    // case "unit":
+    //   out = tests.filter((t) => t.context.match("Unit Tests"));
+    //   break;
+    case "functional":
+      out = tests.filter((t) => t.context.match("Functional Tests"));
       break;
     default:
       out = tests;
